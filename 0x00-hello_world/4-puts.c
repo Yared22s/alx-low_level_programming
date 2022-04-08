333cc900c30ede8752afe4bc26bab786f9109b9a
Yared S <s.yared25@gmail.com>
2022-04-08 16:10:11 +0300

    Betty Style comment added

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,11 @@
 #include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always  (success)
+ */
+
 int main(void)
      {
      puts("\"Programming is like building a multilingual puzzle");
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always  (success)
+ */
 int main(void)
-        {
-	 printf("with proper grammar, but the outcome is a pice of art, \n");
-         return (0);
-	}
+{
+	printf("with proper grammar, but the outcome is a pice of art, \n");
+	return (0);
+}
