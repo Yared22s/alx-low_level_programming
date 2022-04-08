f2ce9d833117b5c69c0820495e51e347888020fc
Yared S <s.yared25@gmail.com>
2022-04-08 15:53:01 +0300

    Task 4,5,6 Minor Corrections made

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main(void)
         {
-	 printf("with proper grammer, but the outcome is a pice of art, \n");
-         return 0;
+	 printf("with proper grammar, but the outcome is a pice of art, \n");
+         return (0);
 	}
