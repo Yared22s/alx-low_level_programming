5340b0b25e9e0bd2994eb7656e2a930f3627645c
Yared S <s.yared25@gmail.com>
2022-04-12 22:01:42 +0300

    Tasks from 0 to 10 is completed

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/*
+ * main - prints the string "_putchar" from a character array.
+ *
+ * return:0 if successfull
+ */
+
+int main(void)
+{
+	char c[] = "_putchar";
+
+	int i;
+
+	for (i=0; i < 8 ; i++)
+	{
+		_putchar(c[i]);
+	}
+	_putchar ('\n');
+	return (0);	
+}
