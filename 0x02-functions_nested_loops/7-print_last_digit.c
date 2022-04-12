77ce272d78f48e716d8317ffe666a12236a68576
Yared S <s.yared25@gmail.com>
2022-04-12 22:47:12 +0300

    Update on Tasks 4,5,6,7,8,9,10

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- * print_last_digit - prints the last digit of a numer.
- * @n: digit to find the last place of.
+ * print_last_digit - prints the last digit of a number.
+ * @n: digit to find the last place.
  * Return: The last digit.
  */
 int print_last_digit(int n)
