24b602f090eb259b31eaa66f620afe04e677159b
Yared S <s.yared25@gmail.com>
2022-04-14 15:13:20 +0300

    Update on task 1

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,5 +1,12 @@
+#ifndef MAIN_H
 #define MAIN_H
 
 #include <stdio.h>
+
 void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+void print_remaining_days(int month, int date, int year);
+int convert_days(int month, int days);
+void print_times_tble(int n);
+
 #endif
