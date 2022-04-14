bbc576f5aa4807dcb1c12a0e1eeb4b7d9fb02296
Yared S <s.yared25@gmail.com>
2022-04-14 14:05:50 +0300

    Debuging Task 1

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+/**
+ * main - tests function that prints if integer is positive or negative
+ * Return: 0 on Success
+ */
+int main(void)
+{
+int i;
+
+i = 0;
+positive_ornegative(i);
+
+return (0);
+}
+
+
+
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,1 @@
+This is where all the debuging codes will be located
