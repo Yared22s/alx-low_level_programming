9e6b78b4676d1d9a67017bf1c0773644d31a7ede
Yared S <s.yared25@gmail.com>
2022-05-20 21:24:34 +0300

    Task Update5

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -13,9 +13,9 @@ size_t print_list(const list_t *h)
 	for (i = 0; h; i++)
 	{
 		if(h->str == NULL)
-		printf("[0] (nil)\n");
+		printf("[%d] (nil)\n");
 		else
-		printf("[%u] %s\n", h->len, h->str);
+		printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
 	}
 
