602f4821a4e09d4084db54d0ab681afe9da29706
Yared S <s.yared25@gmail.com>
2022-05-20 21:58:17 +0300

    Task3 Update

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -29,7 +29,6 @@ void *_strdup(const char *src)
 	dest = malloc((len + 1) * sizeof(char));
 	if (dest == NULL)
 		return (NULL);
-
 	for (i = 0; src[i]; i++)
 		dest[i] = src[i];
 	dest[i] = '\0';
@@ -52,11 +51,12 @@ list_t *add_node_end(list_t **head, const char *str)
 	if (dupstr == NULL)
 		return (NULL);
 	new = malloc(sizeof(list_t));
-		if (new == NULL)	
-	return (NULL);
+	if (new == NULL)
+		return (NULL);
 	new->str = dupstr;
 	new->len = _strlen(str);
-	new->next = NULL;	
+	new->next = NULL;
+
 	if (*head == NULL)
 	{
 		*head = new;
