afcc35442997bdcd328fc89319389d494df2e3b2
Yared S <s.yared25@gmail.com>
2022-05-20 21:56:17 +0300

    Task3 Update

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -56,9 +56,7 @@ list_t *add_node_end(list_t **head, const char *str)
 	return (NULL);
 	new->str = dupstr;
 	new->len = _strlen(str);
-	new->next = NULL;
-	
-
+	new->next = NULL;	
 	if (*head == NULL)
 	{
 		*head = new;
