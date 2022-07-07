c9a4f8d3c04d7a8ac69314775c53e375df56dd7f
Yared S <s.yared25@gmail.com>
2022-07-07 20:25:06 +0300

    update on task 6

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,13 +1,15 @@
 #include "lists.h"
+
 /*
  * sum_dlistint - sum all data (n) in a linked list dlistint_t.
  * @head: Double pointer to struct.
  *
  * Return: 0 if empty or sum of n value
  */
+
 int sum_dlistint(dlistint_t *head)
 {
-if(head)
-	return (head->n + sum_dlistint(head->next));
-return (0);
+	if(head)
+		return (head->n + sum_dlistint(head->next));
+	return (0);
 }
